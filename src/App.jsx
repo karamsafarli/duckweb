b36b93a8322b45b474f0b94d3e41a59f15be8ca9
karamsafarli <karamslee007@gmail.com>
2023-08-28 17:09:26 +0400

    images fixed

--- src/App.jsx
+++ src/App.jsx
@@ -154,7 +154,7 @@ const App = () => {
                   }}
                   className="home_right">
                   <div className="imgcont">
-                    <img src="/src/assets/home.png" alt="" />
+                    <img src="/assets/home.png" alt="" />
                   </div>
                   <div className="home_services">
                     <motion.div
@@ -174,7 +174,7 @@ const App = () => {
                       }}
                       className="home_service">
                       <div className="icon">
-                        <img src="/src/assets/globe.png" alt="" />
+                        <img src="/assets/globe.png" alt="" />
                       </div>
                       <p>{languages[lang].first}</p>
                     </motion.div>
@@ -195,7 +195,7 @@ const App = () => {
                       }}
                       className="home_service">
                       <div className="icon">
-                        <img src="/src/assets/smmicon.png" alt="" />
+                        <img src="/assets/smmicon.png" alt="" />
                       </div>
                       <p>{languages[lang].second}</p>
                     </motion.div>
@@ -216,7 +216,7 @@ const App = () => {
                       }}
                       className="home_service">
                       <div className="icon">
-                        <img src="/src/assets/qricon.png" alt="" />
+                        <img src="/assets/qricon.png" alt="" />
                       </div>
                       <p>{languages[lang].third}</p>
                     </motion.div>
@@ -230,31 +230,31 @@ const App = () => {
         <section ref={aboutSection} id="about">
           <div className="container">
             <div className="row">
-              <motion.div
-                whileInView={{
-                  x: 0,
-                  opacity: 1
-                }}
+              <div className="col-lg-6">
+                <motion.div
+                  whileInView={{
+                    x: 0,
+                    opacity: 1
+                  }}
 
-                initial={{
-                  x: -250,
-                  opacity: 0
-                }}
+                  initial={{
+                    x: -250,
+                    opacity: 0
+                  }}
 
-                transition={{
-                  delay: 0.2,
-                  stiffness: 100,
-                  type: 'spring'
-                }}
+                  transition={{
+                    delay: 0.2,
+                    stiffness: 100,
+                    type: 'spring'
+                  }}
 
-                viewport={{ once: true }}
-                className="col-lg-6">
-                <div className="about_left">
+                  viewport={{ once: true }}
+                  className="about_left">
                   <h3>{languages[lang].heading1}</h3>
                   <h2>{languages[lang].heading2}</h2>
                   <p>{languages[lang].heading3}</p>
-                </div>
-              </motion.div>
+                </motion.div>
+              </div>
               <div
 
                 className="col-lg-6">
@@ -280,7 +280,7 @@ const App = () => {
 
                     viewport={{ once: true }}
                     className="img_cont">
-                    <img src="/src/assets/about_img.png" alt="" />
+                    <img src="/assets/about_img.png" alt="" />
                     <div className="border_div"></div>
                   </motion.div>
 
@@ -316,22 +316,22 @@ const App = () => {
             </div>
 
             <div className="row g-5 justify-content-center">
-              <div className="col-lg-6">
+              <div className="col-lg-5">
                 <motion.div
                   whileInView={{
                     x: 0,
-                    opacity: 1
+                    opacity: 1,
                   }}
 
                   initial={{
                     x: 250,
-                    opacity: 0
+                    opacity: 0,
                   }}
 
                   transition={{
                     delay: 0.2,
                     stiffness: 80,
-                    type: 'spring'
+                    type: 'spring',
                   }}
 
                   viewport={{ once: true }}
@@ -347,12 +347,12 @@ const App = () => {
                           }}
 
                           initial={{
-                            x: 100,
+                            x: 120,
                             opacity: 0
                           }}
 
                           transition={{
-                            delay: (idx + 1) * 0.3,
+                            delay: (idx + 1) * 0.1,
                             stiffness: 60,
                             type: 'spring'
                           }}
@@ -370,8 +370,7 @@ const App = () => {
                   </div>
                 </motion.div>
               </div>
-
-              <div className="col-lg-6">
+              <div className="col-lg-5">
                 <motion.div
                   whileInView={{
                     x: 0,
@@ -395,19 +394,37 @@ const App = () => {
                   <div className="service_options">
                     {
                       languages[lang].services.twopart.options.map((el, idx) => (
-                        <div key={idx} className="service_option">
+                        <motion.div
+                          whileInView={{
+                            x: 0,
+                            opacity: 1
+                          }}
+
+                          initial={{
+                            x: 120,
+                            opacity: 0
+                          }}
+
+                          transition={{
+                            delay: (idx + 1) * 0.1,
+                            stiffness: 60,
+                            type: 'spring'
+                          }}
+
+                          viewport={{ once: true }}
+                          key={idx} className="service_option">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M22 4L12 14.01L9 11.01" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                           </svg>
                           {el}
-                        </div>
+                        </motion.div>
                       ))
                     }
                   </div>
                 </motion.div>
               </div>
-              <div className="col-lg-6">
+              <div className="col-lg-5">
                 <motion.div
                   whileInView={{
                     x: 0,
@@ -431,19 +448,37 @@ const App = () => {
                   <div className="service_options">
                     {
                       languages[lang].services.threepart.options.map((el, idx) => (
-                        <div key={idx} className="service_option">
+                        <motion.div
+                          whileInView={{
+                            x: 0,
+                            opacity: 1
+                          }}
+
+                          initial={{
+                            x: 120,
+                            opacity: 0
+                          }}
+
+                          transition={{
+                            delay: (idx + 1) * 0.1,
+                            stiffness: 60,
+                            type: 'spring'
+                          }}
+
+                          viewport={{ once: true }}
+                          key={idx} className="service_option">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M22 4L12 14.01L9 11.01" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                           </svg>
                           {el}
-                        </div>
+                        </motion.div>
                       ))
                     }
                   </div>
                 </motion.div>
               </div>
-              <div className="col-lg-6">
+              <div className="col-lg-5">
                 <motion.div
                   whileInView={{
                     x: 0,
@@ -467,19 +502,37 @@ const App = () => {
                   <div className="service_options">
                     {
                       languages[lang].services.fourpart.options.map((el, idx) => (
-                        <div key={idx} className="service_option">
+                        <motion.div
+                          whileInView={{
+                            x: 0,
+                            opacity: 1
+                          }}
+
+                          initial={{
+                            x: 120,
+                            opacity: 0
+                          }}
+
+                          transition={{
+                            delay: (idx + 1) * 0.1,
+                            stiffness: 60,
+                            type: 'spring'
+                          }}
+
+                          viewport={{ once: true }}
+                          key={idx} className="service_option">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M22 4L12 14.01L9 11.01" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                           </svg>
                           {el}
-                        </div>
+                        </motion.div>
                       ))
                     }
                   </div>
                 </motion.div>
               </div>
-              <div className="col-lg-6">
+              <div className="col-lg-5">
                 <motion.div
                   whileInView={{
                     x: 0,
@@ -503,13 +556,31 @@ const App = () => {
                   <div className="service_options">
                     {
                       languages[lang].services.fivepart.options.map((el, idx) => (
-                        <div key={idx} className="service_option">
+                        <motion.div
+                          whileInView={{
+                            x: 0,
+                            opacity: 1
+                          }}
+
+                          initial={{
+                            x: 120,
+                            opacity: 0
+                          }}
+
+                          transition={{
+                            delay: (idx + 1) * 0.1,
+                            stiffness: 60,
+                            type: 'spring'
+                          }}
+
+                          viewport={{ once: true }}
+                          key={idx} className="service_option">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M22 4L12 14.01L9 11.01" stroke="#FFD002" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                           </svg>
                           {el}
-                        </div>
+                        </motion.div>
                       ))
                     }
                   </div>
